dabb9c7c67da45636d5eae990319528699d46671
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2020-12-18 13:32:27 -0300

    Desafio 09 - Soundboard

---
+++ 09-soundboard/scripts.js
@@ -0,0 +1,14 @@
+const btns = document.querySelectorAll(".btn");
+const path = "/sounds/"
+
+let audioFile = new Audio();
+
+btns.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        audioFile.pause();
+        audioFile.currentTime = 0;
+        const audio = item.dataset.audio;
+        audioFile = new Audio(`${path}${audio}.mp3`);
+        audioFile.play();
+    })
+});
