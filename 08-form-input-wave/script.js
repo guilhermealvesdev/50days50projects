5b5b7bea68883d587498591980295a8964b601d3
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2020-12-26 21:51:12 -0300

    Efeitinho de borda

--- 08-form-input-wave/script.js
+++ 08-form-input-wave/script.js
@@ -12,3 +12,20 @@ labelField.forEach((item) => {
         item.appendChild(spanLetra);
     });
 });
+
+//Efeito de caixas
+const caixa1 = document.querySelector('.caixa');
+const caixa2 = document.querySelector('.caixa2');
+const SPEED = 2;
+
+document.addEventListener('mousemove', (e) =>{
+    mexeCaixa(e, caixa1, SPEED);
+    mexeCaixa(e, caixa2, SPEED);
+});
+
+const mexeCaixa = (e, elemento, velocidade) => {
+    let x = (window.innerWidth - e.pageX * velocidade) / 50;
+    let y = (window.innerHeight - e.pageY * velocidade) / 50;
+
+    elemento.style.transform = `translateX(${x}px) translateY(${y}px)`
+}
